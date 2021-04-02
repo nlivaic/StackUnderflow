0db2143566f6fcc70bff1b1b35286cc483fbf07b
Nenad Livaic <nenad.livaic@gmail.com>
2021-04-02 14:27:38 +0200

    dev: DbUp Migrations with DDL and DML scripts.

---
+++ src/StackUnderflow.Migrations/Program.cs
@@ -0,0 +1,63 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Compumed.Migrations
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+                ?? "Development";
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            var connectionString =
+                args.FirstOrDefault()
+                ?? config["ConnectionStrings:CompumedDB"];
+
+            string scriptsPath = null;
+            if (args.Length == 2)
+            {
+                scriptsPath = args[1];
+            }
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem(scriptsPath != null ? Path.Combine(scriptsPath, "Scripts") : Path.Combine(Environment.CurrentDirectory, "Scripts"))
+                    .LogToConsole()
+                    .Build();
+            // Comment the following two lines out
+            // before the first run ONLY in case you want
+            // to create the database from scratch.
+            upgrader.MarkAsExecuted("0001_schema.sql");
+            upgrader.MarkAsExecuted("0002_data.sql");
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ src/StackUnderflow.Migrations/readme.md
@@ -0,0 +1,12 @@
+﻿#### How to migrate the database
+
+##### Development
+
+No need to do anything. When the solution is ran Docker will run both the database container and 
+DbUp migration container. Migration container will wait for the database to spin up and then 
+will execute the migrations.
+
+##### Production
+
+Configure connection string using CLI arguments. First argument should be a string with the entire
+connection string.
