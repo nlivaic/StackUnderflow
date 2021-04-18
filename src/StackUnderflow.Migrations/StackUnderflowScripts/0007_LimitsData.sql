630b4301fd5bafa865462270b6e277dd69651908
nenad.livaic <nenad.livaic@five.agency>
2021-04-18 21:55:16 +0200

    dev: Populate limits data.

---
+++ src/StackUnderflow.Migrations/StackUnderflowScripts/0007_LimitsData.sql
@@ -0,0 +1,38 @@
+﻿INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('AnswerEditDeadline', '10');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('QuestionEditDeadline', '10');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('CommentEditDeadline', '10');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('VoteEditDeadline', '10');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('AcceptAnswerDeadline', '10');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('QuestionBodyMinimumLength', '100');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('AnswerBodyMinimumLength', '100');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('CommentBodyMinimumLength', '30');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('TagMinimumCount', '1');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('TagMaximumCount', '5');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('UsernameMinimumLength', '5');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('UsernameMaximumLength', '15');
+
+INSERT INTO "LimitsKeyValuePairs" ("LimitKey", "LimitValue")
+VALUES ('AboutMeMaximumLength', '400');
